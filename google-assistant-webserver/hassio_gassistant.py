728d5256d718434f484122c1915ed30a3eb1dc4a
dtrung2015 <57569530+dtrung2015@users.noreply.github.com>
2021-07-23 20:30:03 +0900

    Update hassio_gassistant.py

--- google-assistant-webserver/hassio_gassistant.py
+++ google-assistant-webserver/hassio_gassistant.py
@@ -1,4 +1,11 @@
 """Hass.IO Google Assistant."""
+from actions import say
+from flask import Flask, request
+from flask_restful import Resource, Api
+import threading
+app = Flask(__name__)
+api = Api(app)
+
 import json
 import sys
 from pathlib import Path
@@ -153,3 +160,19 @@ if __name__ == '__main__':
     assistant = GoogleTextAssistant('vi-VN', 'HA_GA', 'HA_GA_TEXT_SERVER',
                              True, grpc_channel, DEFAULT_GRPC_DEADLINE)
     app.run(host='0.0.0.0')
+def start_server():
+    app.run(host='0.0.0.0',port=5001)
+
+def main():
+        class tts(Resource):
+            def get(self):
+                message = request.args.get('message', default = 'This is a test!')
+                say(message)
+                return {'status': 'OK'}
+        api.add_resource(tts, '/tts')
+        server = threading.Thread(target=start_server,args=())
+        server.setDaemon(True)
+        server.start()
+
+if __name__ == '__main__':
+    main()
